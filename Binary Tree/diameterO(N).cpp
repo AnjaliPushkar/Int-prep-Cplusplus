5a7b8a494b7440b9a1e522ed92fad282228cd6b0
Anjali Pushkar <anjalipushkar15@gmail.com>
2020-10-11 03:16:25 +0530

    better approach to find heigth, diamtr

---
+++ Binary Tree/diameterO(N).cpp
@@ -0,0 +1,74 @@
+#include <iostream>
+#include <algorithm>
+#include <queue>
+using namespace std;
+
+class node{
+    public:
+        int data;
+        node *left;
+        node*right;
+
+        node(int d){
+            data = d;
+            left = NULL;
+            right = NULL;
+        }
+}; 
+
+node *buildTree(){
+    int d;
+    cin>>d;
+
+    if(d == -1){
+        return NULL;
+    }
+    node *root = new node(d);
+    root->left = buildTree();
+    root->right = buildTree();
+    return root;
+}
+
+int height(node*root){
+    if(root == NULL){
+        return 0;
+    }
+    int ls = height(root->left);
+    int rs = height(root->right);
+    return max(ls, rs) +1;
+
+}
+
+class Pair{
+    public:
+        int height;
+        int diameter;
+};
+
+Pair fastDiameter(node*root){
+    Pair p;
+    if(root == NULL){
+        p.diameter = p.height = 0;
+        return p;
+    }
+    Pair left = fastDiameter(root->left);
+    Pair right = fastDiameter(root->right);
+    p.height = max(left.height, right.height) + 1;
+    p.diameter = max(left.height + right.height, max(left.diameter, right.diameter));
+    return p;
+}
+
+int main() {
+    node *root  = buildTree();
+    Pair p = fastDiameter(root);
+    cout<<p.height<<endl;
+    cout<<p.diameter<<endl;
+    return 0;
+}
+/*
+Input
+8 10 1 -1 -1 6 9 -1 -1 7 -1 -1 3 -1 14 13 -1 -1 -1
+Output
+4
+6
+*/
